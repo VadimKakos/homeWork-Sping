cd7f77ac175e6b385b2b24ccb3fbfed669813ff6
vadimkosolapov <vilgutis@icloud.com>
2023-01-25 10:55:01 +0500

    done

--- src/main/java/com/example/homeworksping/service/CartService.java
+++ src/main/java/com/example/homeworksping/service/CartService.java
@@ -1,4 +1,24 @@
 package com.example.homeworksping.service;
 
+import com.example.homeworksping.reporitory.CartBasket;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CartService {
+
+    private final CartBasket basket;
+
+    public CartService(CartBasket cartBasket) {
+        this.basket = cartBasket;
+    }
+
+    public void addToCart(List<Integer> ids) {
+        this.basket.addToCart(ids);
+    }
+
+    public List<Integer> getCart() {
+        return this.basket.getCart();
+    }
 }
